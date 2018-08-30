c458841ea1027f35d192b5e6c2bea0f43ee9070c
Gene <gn02336853@gmail.com>
2018-08-30 20:21:52 +0800

    second commit

--- install_packages.sh
+++ install_packages.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-apt_get_packages = "python-qt4 mongodb-clients"
 sudo apt-get update
 sudo pip install tensorflow
 sudo pip install pymongo
 sudo pip install progressbar
 sudo pip install pyqtgraph
-sudo apt-get install  -y $apt_get_packages
+sudo pip install scipy
+sudo pip install matplotlib
+sudo apt-get install python-qt4
+sudo apt-get install mongodb-clients
+sudo apt-get install python-tk
